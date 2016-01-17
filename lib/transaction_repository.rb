e714765d2f655fae9026b46e8e8b1527a910a5d8
Alireza Andersen <alireza.andersen@gmail.com>
2016-01-17 15:23:02 -0700

    Iteration_03 with transaction, repo, and test

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -4,6 +4,53 @@ require 'invoice_items'
 
 class TransactionRepository
 
-  attr_reader :
+  attr_reader :transactions
+
+
+  def initialize
+    @transactions = []
+  end
+
+
+  def load_data(filename)
+    data = CSV.open filename, headers: true, header_converters: :symbol
+
+    data.each do |row|
+      transactions << Transaction.new( {:id => row[:id].to_i,
+      :invoice_id => row[:invoice_id],
+      :credit_card_number => row[:credit_card_number],
+      :credit_card_expiration_date => row[:credit_card_expiration_date],
+      :result => row[:result],
+      :created_at => Time.parse(row[:created_at]),
+      :updated_at => Time.parse(row[:updated_at])
+      } )
+    end
+  end
+
+  def load_transactions(trans)
+    trans.each do |invoice|
+      transactions << invoice
+    end
+  end
+
+  def all
+    transactions
+  end
+
+  def find_by_id(id_number) #should it be invoice item #
+    transactions.find { |invoice| invoice.id == id_number }
+  end
+
+  def find_all_by_invoice_id(invoice_num)
+    transactions.find_all { |invoice| invoice.invoice_id == invoice_num }
+  end
+
+  def find_all_by_credit_card_number(cc_num)
+    transactions.find_all { |invoice| invoice.credit_card_number == cc_num }
+  end
+
+  def find_all_by_result(status)
+    transactions.find_all { |invoice| invoice.result == status }
+  end
 
 end
--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -1,9 +1,161 @@
 require 'test_helper'
-require 'transaction'
+require 'transaction_repository'
 require 'csv'
 require 'invoice'
 
 
 class TransactionRepositoryTest < Minitest::Test
 
-end 
+  attr_reader :c_time, :u_time
+
+  def setup
+    @c_time = Time.new(2016, 01, 04, 11, 27, 39, "-07:00")
+    @u_time = Time.new(2016, 01, 25, 05, 12, 50, "-07:00")
+  end
+
+  def test_will_load_transactions_from_a_file
+    trans = TransactionRepository.new
+    trans.load_data("./data/transactions.csv")
+    assert_equal 4985, trans.all.length
+  end
+
+  def test_transaction_will_return_an_all_by_invoice_id
+
+    trans1 = Transaction.new({
+    :id => 2,
+    :invoice_id => 3,
+    :credit_card_number => "5151424242424242",
+    :credit_card_expiration_date => "0220",
+    :result => "success",
+    :created_at => c_time,
+    :updated_at => u_time
+    })
+    trans2 = Transaction.new({
+    :id => 6,
+    :invoice_id => 8,
+    :credit_card_number => "5353424242424242",
+    :credit_card_expiration_date => "0220",
+    :result => "success",
+    :created_at => c_time,
+    :updated_at => u_time
+    })
+    trans3 = Transaction.new({
+    :id => 6,
+    :invoice_id => 8,
+    :credit_card_number => "5252424242424242",
+    :credit_card_expiration_date => "0220",
+    :result => "success",
+    :created_at => c_time,
+    :updated_at => u_time
+    })
+    trans = TransactionRepository.new
+    trans.load_transactions([trans1, trans2, trans3])
+
+    assert_equal [trans2, trans3], trans.find_all_by_invoice_id(8)
+  end
+
+  def test_transaction_will_return_a_transactions_with_a_vaild_result_status
+
+    trans1 = Transaction.new({
+    :id => 2,
+    :invoice_id => 3,
+    :credit_card_number => "5151424242424242",
+    :credit_card_expiration_date => "0220",
+    :result => "success",
+    :created_at => c_time,
+    :updated_at => u_time
+    })
+    trans2 = Transaction.new({
+    :id => 6,
+    :invoice_id => 8,
+    :credit_card_number => "5252424242424242",
+    :credit_card_expiration_date => "0220",
+    :result => "success",
+    :created_at => c_time,
+    :updated_at => u_time
+    })
+    trans3 = Transaction.new({
+    :id => 6,
+    :invoice_id => 8,
+    :credit_card_number => "5252424242424242",
+    :credit_card_expiration_date => "0220",
+    :result => "failed",
+    :created_at => c_time,
+    :updated_at => u_time
+    })
+    trans = TransactionRepository.new
+    trans.load_transactions([trans1, trans2, trans3])
+
+    assert_equal [trans1,trans2], trans.find_all_by_result("success")
+  end
+
+  def test_transaction_will_return_a_transactions_with_a_vaild_CC_number
+
+    trans1 = Transaction.new({
+    :id => 2,
+    :invoice_id => 3,
+    :credit_card_number => "5151424242424242",
+    :credit_card_expiration_date => "0220",
+    :result => "success",
+    :created_at => c_time,
+    :updated_at => u_time
+    })
+    trans2 = Transaction.new({
+    :id => 6,
+    :invoice_id => 8,
+    :credit_card_number => "5252424242424242",
+    :credit_card_expiration_date => "0220",
+    :result => "success",
+    :created_at => c_time,
+    :updated_at => u_time
+    })
+    trans3 = Transaction.new({
+    :id => 6,
+    :invoice_id => 8,
+    :credit_card_number => "5252424242424242",
+    :credit_card_expiration_date => "0220",
+    :result => "failed",
+    :created_at => c_time,
+    :updated_at => u_time
+    })
+    trans = TransactionRepository.new
+    trans.load_transactions([trans1, trans2, trans3])
+
+    assert_equal [trans2,trans3], trans.find_all_by_credit_card_number("5252424242424242")
+  end
+
+  def test_transaction_will_return_a_transactions_with_a_vaild_CC_number
+
+    trans1 = Transaction.new({
+    :id => 2,
+    :invoice_id => 3,
+    :credit_card_number => "5151424242424242",
+    :credit_card_expiration_date => "0220",
+    :result => "success",
+    :created_at => c_time,
+    :updated_at => u_time
+    })
+    trans2 = Transaction.new({
+    :id => 6,
+    :invoice_id => 8,
+    :credit_card_number => "5252424242424242",
+    :credit_card_expiration_date => "0220",
+    :result => "success",
+    :created_at => c_time,
+    :updated_at => u_time
+    })
+    trans3 = Transaction.new({
+    :id => 6,
+    :invoice_id => 8,
+    :credit_card_number => "5252424242424242",
+    :credit_card_expiration_date => "0220",
+    :result => "failed",
+    :created_at => c_time,
+    :updated_at => u_time
+    })
+    trans = TransactionRepository.new
+    trans.load_transactions([trans1, trans2, trans3])
+
+    assert_equal [trans2,trans3], trans.find_all_by_credit_card_number("5252424242424242")
+  end
+end
