07ee8b4e691eb61b8d516e2a176ff20101b77c28
Alireza Andersen <alireza.andersen@gmail.com>
2016-01-17 16:14:37 -0700

    Customer Service and Test

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,10 +2,10 @@ class Customer
 
   attr_reader :id, :first_name, :last_name, :created_at, :updated_at
 
-  def initalize(args)
+  def initialize(args)
     @id = args[:id].to_i
-    @first_name = args[:first_name]
-    @last_name = args[:last_name]
+    @first_name = args[:first_name].capitalize
+    @last_name = args[:last_name].capitalize
     @created_at = args[:created_at]
     @updated_at = args[:updated_at]
   end
